86622b03a05fadb27027c7742bbabc6ea48f4659
0xB370 <mansilla_alberto@outlook.com>
2024-05-05 16:20:02 -0300

    Layout First Approach

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 const WeatherApp = () => {
+
+  const [ciudad, setCiudad] = useState('')
+
+  const handleCambioCiudad = (e) => {
+    setCiudad(e.target.value)
+  }
+
   return (
     <div className="container">
-        <h1>Aplicación del Clima</h1>
+      <h1>Aplicación del Clima</h1>
+      <form>
+        <input 
+          type="text" 
+          value={ciudad}
+          onChange={handleCambioCiudad}
+        />
+        <button type="submit">Buscar</button>
+      </form>
     </div>
   )
 }
